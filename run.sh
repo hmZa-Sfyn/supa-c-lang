541d8b98d193b8ee9c9fb8c7496a5e83323f6c0d
hmZa-Sfyn <hamza.enchant@gmail.com>
2025-03-31 13:59:50 +0500

    commit-2025-03-31 13:59:50.699960

--- run.sh
+++ run.sh
@@ -1,3 +1,31 @@
 ## start and put alias of the lang things
 
+$pwd = $(pwd)
 
+## copy things to root
+
+
+## create dirs first
+mkdir /usr/local/bin/supac
+mkdir /usr/local/bin/supac/bin
+mkdir /usr/local/bin/supac/libs
+
+## create some files too
+echo $(cat ./run.sh) >> /usr/local/bin/supac/supac.sh
+
+echo "LANGUAGE::VERSION=1.1" >> /usr/local/bin/supac/supac.env
+echo "LANGUAGE::IS_BETA=True" >> /usr/local/bin/supac/supac.env
+
+## copy them now
+cp ./supac /usr/local/bin/supac/bin/
+
+cp ./assets /usr/local/bin/supac
+cp ./stdlib /usr/local/bin/supac/libs
+
+## for language itself
+
+echo "alias supacc=''"
+
+## for language manager
+
+## for package manager
